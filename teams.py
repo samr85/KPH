1e361300e625d851378358a2dcbfd81adfb666e8
will <will@>
2018-08-06 20:46:48 +0100

    Support for long team names
    Using team.fullName on the scoreboard.
    Using shortened username (team.name) on login to prevent teams having to
    scroll past very long team names

--- teams.py
+++ teams.py
@@ -83,7 +83,7 @@ def datetimeToJsString(dtime):
 
 class TeamScore:
     def __init__(self, team):
-        self.name = team.name
+        self.name = team.fullName
         self.time, self.score = team.getScore()
 
 class TeamList:
