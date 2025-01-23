e8d6f6dd5edc9920a0b8c69cb1a849ffb9218544
Hannah Poyner <hannahpoyner@Hannahs-MacBook-Air-2.local>
2025-01-23 19:50:54 +0000

    Adding a readme and exampleTeams.py

---
+++ exampleTeams.py
@@ -0,0 +1,4 @@
+teams = [("ShortNm",           "random password",    "Less short more long name")]
+
+# A bit hacky, but this seems like a good place to put the admin password
+admin = "example example
